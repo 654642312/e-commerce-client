78a6875c8abdbfe74b9e7caf81859050522d8e60
Edgar018 <edgar_belandria010@hotmail.com>
2022-06-12 08:03:54 -0400

    add banner in home page

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -3,6 +3,8 @@ import CardContainer from "../../components/card-container/card-container";
 import useFetch from "../../hooks/useFetch";
 import { getBooks } from "../../services/books/books";
 import Loading from "../../components/loading/loading";
+import Banner from '../../assets/banner.jpg';
+import './styles.css'
 
 function Home() {
   const [books, loading] = useFetch(getBooks);
@@ -10,17 +12,22 @@ function Home() {
   if (loading) return <Loading />;
 
   return (
-    <CardContainer>
-      {books?.map((book) => (
-        <Card
-          key={book.id}
-          img={book.image}
-          title={book.title}
-          id={book.id}
-          price={book.price}
-        />
-      ))}
-    </CardContainer>
+    <div>
+      <div className="banner">
+        <img src={Banner} alt="banner" />
+      </div>
+      <CardContainer>
+        {books?.map((book) => (
+          <Card
+            key={book.id}
+            img={book.image}
+            title={book.title}
+            id={book.id}
+            price={book.price}
+          />
+        ))}
+      </CardContainer>
+    </div>
   );
 }
 
